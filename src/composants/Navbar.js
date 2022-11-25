3faa072de8865023622ac5da0f9a8add54a1f0b7
Benoit Auger-Dubois <benoit.auger--dubois@etu.univ-nantes.fr>
2022-11-25 11:41:33 +0100

    feature : navbar

--- src/composants/Navbar.js
+++ src/composants/Navbar.js
@@ -3,12 +3,18 @@ export default function Navbar(){
     return <nav className="nav">
     <a href="/" className="site-title">Magic Fan Site</a>
         <ul>
-            <li>
-                <a href="/Information">About Page</a>
-            </li>
-            <li>
-                <a href="/MyForm">Find a card</a>
-            </li>
+            <CustomLink href="/Information">About Page</CustomLink>
+            <CustomLink href="/MyForm">Find a card</CustomLink>
         </ul>
     </nav>
+}
+
+
+function CustomLink( { href, children, ...props } ) {
+    const path = window.location.pathname
+    return (
+        <li className={path === href ? "active" : ""}>
+            <a href={href} {...props}>{children}</a>
+        </li>
+    )
 }
