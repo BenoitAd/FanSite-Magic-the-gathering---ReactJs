8f95d8d6a086d6a37f2956b81f5cce7b6dce7e9a
Benoit.ad <62358946+BenoitAd@users.noreply.github.com>
2022-11-30 10:43:03 +0100

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ function HomePage() {
     const navigate = useNavigate();
     return(
     <div >
-    <h1 className='pageTitle'>Magic Fan Club</h1>
+    <h1 className='pageTitle'>Magic Fan Site</h1>
     <div className='div'>
         <button className='buttonAcceuil' onClick={() => navigate(`/MyForm`)}>Find your card by filter</button>
         <button className='buttonAcceuil' onClick={() => navigate(`/Information`)}>About page</button>
